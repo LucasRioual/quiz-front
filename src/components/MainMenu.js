678b64afd59f157f6063b44d153f927b508ba31f
LucasRioual <lucas.rioual@centrale-marseille.fr>
2024-03-10 19:27:02 +0100

    changement

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MainMenu(props) {
   return (
     <div className="font-Lexen">
-        <h2 onClick={props.create} className="duration-75 text-4xl md:text-5xl my-10 text-white hover:-translate-y-2 hover:text-primary cursor-pointer font-bold">Créer une partie</h2>
+        <h2 onClick={props.create} className="duration-75 text-5xl md:text-5xl my-10 text-white hover:-translate-y-2 hover:text-primary cursor-pointer font-bold">Créer une partie</h2>
         <h2 onClick={props.join} className="duration-75 text-4xl  md:text-5xl my-10 text-white hover:-translate-y-2 hover:text-primary cursor-pointer font-bold">Rejoindre une partie</h2>
         <h2 onClick={props.regle} className="duration-75 text-3xl md:text-4xl  my-10 text-white hover:-translate-y-2 hover:text-primary cursor-pointer">Comment jouer ?</h2>
     </div>
